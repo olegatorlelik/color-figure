900c98cc90ff253c9a8f377cd8055a41f80ab06e
igorevich <lelik.belokonevvv@gmail.com>
2020-11-29 13:35:11 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Figure from "./components/Circle/Figure";
 import Nav from "./components/Navigation/Nav";
 
 function App(props) {
   return (
     <div className="App">
-      <div>
+      <BrowserRouter>
         <Nav />
-      </div>
+        <Switch>
+          <Route
+            exact
+            path="/figure/:figure/:color"
+            render={(props) => {
+              <Figure {...props} />;
+            }}
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Circle/Figure.js
+++ src/components/Circle/Figure.js
@@ -1,32 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
 import "./StyleFigure.css";
-import { Link } from "react-router-dom";
 
 const Figure = (props) => {
-  const figures = ["circle", "square"];
-  const colors = ["red", "black", "yellow", "green", "gray"];
-
-  const [figureState, setState] = useState("");
-  const [colorState, setColorState] = useState("");
-  console.log(figureState);
-
+  console.log(useParams());
   return (
     <div>
-      {figures.map((elem, index) => {
-        return (
-          <Link to={`${elem}`} key={index} onClick={() => setState(elem)}>
-            {elem}{" "}
-          </Link>
-        );
-      })}
-      <div className={figureState + " " + colorState}></div>
-      {colors.map((elem, index) => {
-        return (
-          <Link to={`${elem}`} key={index} onClick={() => setColorState(elem)}>
-            {elem}{" "}
-          </Link>
-        );
-      })}
+      <div className={props.figure + " " + props.color}>{props.figure}</div>
     </div>
   );
 };
